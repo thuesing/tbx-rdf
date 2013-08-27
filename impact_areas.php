2fa2e1e3eee2895027e3af38bbe4c3d447a04dde
thuesing <t.huesing@gmx.de>
2013-08-27 15:23:38 +0200

    add in skos:topConcept and ia namespace

--- impact_areas.php
+++ impact_areas.php
@@ -14,12 +14,15 @@ require_once "easyrdf/lib/EasyRdf.php";
 
 # set namespace
 EasyRdf_Namespace::set('tbx', 'http://beta.liaise-toolbox.eu/');
+EasyRdf_Namespace::set('ia', 'http://beta.liaise-toolbox.eu/impact_assessment#');
 
 $graph = new EasyRdf_Graph();
 
-$taxo_machine_name = 'economic_impacts';
+$taxos = array('economic_impacts','environmental_impacts','social_impacts');
+//$taxo_machine_name = 'economic_impacts';
+_graph_setup(&$graph, $taxos);
 
-_graph_add_taxo($graph, $taxo_machine_name);
+//_graph_add_taxo($graph, $taxo_machine_name);
 
 // serialize to
 // other formats: ntriples, rdfxml, turtle
@@ -32,7 +35,7 @@ print($data);
 
 function _graph_add_taxo(&$graph, $taxo_machine_name) {
     $voc = taxonomy_vocabulary_machine_name_load($taxo_machine_name);
-    // $voc->hierarchy 1/0
+    
     $tree = taxonomy_get_tree($voc->vid);
     $tree = entity_key_array_by_property($tree, 'tid');
     $items = array();
@@ -47,110 +50,66 @@ function _graph_add_taxo(&$graph, $taxo_machine_name) {
       $graph->addLiteral($resource, 'skos:prefLabel', $term->name, 'en');
          
       foreach ($term->parents as $parent_id) {
-        if($parent_id == 0) continue; // root item    
-        $parent = $tree[$parent_id];
-        $parent->name = str_replace(' ', '-', $parent->name);
-        $graph->addResource($resource, 'skos:broader', 'tbx:'.$parent->name); 
-
-      }
-    }  
-}
-
-
-/*
- * @params $graph easyrdf graph obj
- * @params $item json item
- */
-function map_item(&$graph, &$item) { 
-    global $namespaces, $mappings;
- 
-
-    # unset unused text etc
-    unset($item['label']);
-    unset($item['created']); # we use changed instead
-    unset($item['field_body']);
-    unset($item['field_enduser_documentation']);
-    unset($item['field_scientific_documentation']);
-    unset($item['field_input']);
-    unset($item['field_output']);
-    unset($item['field_contact']);
-
-    # process item 
-
-    // replace Drupal namespace
-    $item['url'] = str_replace('/models/','', $item['url']);
-
-    $resource = 'tbx:'.$item['url']; # single resource URI
-    $type = 'tbx:'.$item['type'];
-    unset($item['url']);
-    unset($item['type']);
-
-    #$graph->setType('tbx:'.$item['url'],  'tbx:'.$item['type']); same as
-    $graph->addResource($resource, "rdf:type", $type);
-    $graph->addType($resource, array('sioc:Item', 'foaf:Document')); #here
-
-    # set editor if null
-    $item['editor'] = isset($item['editor']) ? $item['editor'] : $item['author'];
-    $graph->addResource($resource, "sioc:has_creator", 'tbx:' . $item['editor']);
-    unset($item['editor']);
-    unset($item['author']);
-    
-    # map title
-    $graph->addLiteral($resource, 'dc:title', $item['name']);
-    unset($item['name']);
-    
-    # map created
-    $graph->addLiteral($resource, 'dc:date', $item['changed']);
-    $graph->addLiteral($resource, 'dc:created', $item['changed']);
-    unset($item['changed']);
-    
-    if(isset($item['field_website_for_contact'])) {
-        $graph->addLiteral($resource, 'foaf:homepage', $item['field_website_for_contact']);
-        unset($item['field_website_for_contact']);
-    }
-    
-  # end preprocess item 
-
-  # process plain literals
-    foreach ($item as $key => &$values) {
-        if(!is_array($values)) {
-            $values = array($values); 
-        } 
-        foreach ($values as $val) {
-            $graph->addLiteral($resource, 'tbx:'.$key, $val);
+        if($parent_id == 0) { // root item  set relation to top Concept, per convention taxo_machine_name s.U.
+            $graph->addResource($resource, "skos:broader", 'ia:' . $taxo_machine_name);
+        } else {  // set relation to Parent concept
+            $parent = $tree[$parent_id];
+            $parent->name = str_replace(' ', '-', $parent->name);
+            $graph->addResource($resource, 'skos:broader', 'tbx:'.$parent->name); 
         }
-    }
 
-}
-
-
-
-/*
- * @return array of hierarchical term items in simile exhibit format
- */
-function _terms_for($taxo_machine_name){
-
-    $voc = taxonomy_vocabulary_machine_name_load($taxo_machine_name);
-    // $voc->hierarchy 1/0
-    $tree = taxonomy_get_tree($voc->vid);
-    $tree = entity_key_array_by_property($tree, 'tid');
-    $items = array();
-
-    foreach ($tree as $term) {
-      foreach ($term->parents as $parent_id) {
-        if($parent_id == 0) continue; // root item
-        $parent = $tree[$parent_id];
-        $item = array();
-        $item['type'] = $voc->machine_name;
-        $item['label'] = $term->name;
-        $item['subtopicOf'] = $parent->name;
-        if(empty($item['subtopicOf'])) unset($item['subtopicOf']); 
-        $items[] = $item;
       }
     }  
-
-    return $items;
-
 }
 
+function _graph_setup(&$graph, $taxos) {
+
+/*
+<> a skos:ConceptScheme ;
+  skos:prefLabel "impact assessment"@en ;
+  skos:hasTopConcept <impact areas> .
+*/
+
+ $resource = 'ia:'. 'http://beta.liaise-toolbox.eu/impact_assessment#';
+ $graph->addType($resource, array('skos:ConceptScheme'));
+ $graph->addResource($resource, "skos:hasTopConcept", 'ia:impact_areas');
+
+/*
+<impact areas>
+  a  skos:Concept;
+  skos:prefLabel "impact areas";
+  skos:altLabel "impact areas";
+  skos:definition "Description of impact areas.";
+  skos:narrower <social impacts> ;
+  skos:narrower <environmental impacts> ;
+  skos:narrower <economic impacts>;
+  skos:inScheme <> .
+*/
+
+
+ $resource = 'ia:impact_areas';
+ $graph->addType($resource, array('skos:Concept'));
+ $graph->addLiteral($resource, 'skos:prefLabel', 'impact areas', 'en');
+ $graph->addLiteral($resource, 'skos:definition', 'TODO Description of impact areas.', 'en');
+
+/*
+ $graph->addResource($resource, "skos:narrower", 'ia:economic_impacts']);
+ $graph->addResource($resource, "skos:narrower", 'ia:environmental_impacts']);
+ $graph->addResource($resource, "skos:narrower", 'ia:social_impacts']);
+*/
+/* single taxos */
+
+
+foreach ($taxos as $name) {
+     $resource = 'ia:' . $name;
+     $graph->addType($resource, array('skos:Concept'));
+     $graph->addLiteral($resource, 'skos:prefLabel', $name , 'en');
+     $graph->addLiteral($resource, 'skos:definition', 'TODO Description of impact areas.', 'en');
+     $graph->addResource($resource, "skos:broader", 'ia:impact_areas');
+}
+
+
+
+
+}
 ?>
