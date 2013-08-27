e96b041caab3c921676725dfed5d803f4cf86814
thuesing <t.huesing@gmx.de>
2013-08-27 14:40:49 +0200

    taxo to graph refactored to method

--- impact_areas.php
+++ impact_areas.php
@@ -19,38 +19,43 @@ $graph = new EasyRdf_Graph();
 
 $taxo_machine_name = 'economic_impacts';
 
+_graph_add_taxo($graph, $taxo_machine_name);
 
-$voc = taxonomy_vocabulary_machine_name_load($taxo_machine_name);
-// $voc->hierarchy 1/0
-$tree = taxonomy_get_tree($voc->vid);
-$tree = entity_key_array_by_property($tree, 'tid');
-$items = array();
-
-foreach ($tree as $term) {
-
-  $term->name = str_replace(' ', '-', $term->name);
-
-  $resource = 'tbx:'.$term->name; # single resource URI
-  // $graph->addResource($resource, "rdf:type", 'owl:Class');
-  $graph->addType($resource, array('skos:Concept','owl:Class')); #here
-  $graph->addLiteral($resource, 'skos:prefLabel', $term->name, 'en');
-     
-  foreach ($term->parents as $parent_id) {
-    if($parent_id == 0) continue; // root item    
-    $parent = $tree[$parent_id];
-    $parent->name = str_replace(' ', '-', $parent->name);
-    $graph->addResource($resource, 'skos:broader', 'tbx:'.$parent->name); 
-
-  }
-}  
-
+// serialize to
 // other formats: ntriples, rdfxml, turtle
 $data = $graph->serialise('rdfxml');
 if (!is_scalar($data)) {
     $data = var_export($data, true);
 }
-
 print($data);
+
+
+function _graph_add_taxo(&$graph, $taxo_machine_name) {
+    $voc = taxonomy_vocabulary_machine_name_load($taxo_machine_name);
+    // $voc->hierarchy 1/0
+    $tree = taxonomy_get_tree($voc->vid);
+    $tree = entity_key_array_by_property($tree, 'tid');
+    $items = array();
+
+    foreach ($tree as $term) {
+
+      $term->name = str_replace(' ', '-', $term->name);
+
+      $resource = 'tbx:'.$term->name; # single resource URI
+      // $graph->addResource($resource, "rdf:type", 'owl:Class');
+      $graph->addType($resource, array('skos:Concept','owl:Class')); #here
+      $graph->addLiteral($resource, 'skos:prefLabel', $term->name, 'en');
+         
+      foreach ($term->parents as $parent_id) {
+        if($parent_id == 0) continue; // root item    
+        $parent = $tree[$parent_id];
+        $parent->name = str_replace(' ', '-', $parent->name);
+        $graph->addResource($resource, 'skos:broader', 'tbx:'.$parent->name); 
+
+      }
+    }  
+}
+
 
 /*
  * @params $graph easyrdf graph obj
